2ae9ebb8e2114c168a98083301cc2e29c6c48d08
guilherme.petry <guilhermepetryrs@gmail.com>
2021-10-08 15:31:14 -0300

    implemented prototype operator assignment

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator")
 const header = document.querySelector("h1");
 const clearButton = document.querySelector(".clear");
 
@@ -20,22 +21,53 @@ function operate(a, b, operator) {
   }
 }
 
-function display(e) {
-  const number = e.target.textContent;
-  if (header.textContent.length <= 14)
-    if (header.textContent === "0") {
-      header.textContent = number;
-    } else {
-      header.textContent = `${header.textContent}${number}`;
+const operation = {
+  first : 0,
+  second : 0,
+  operator : "",
+}
+
+function displayNumber(e) {
+  if (header.textContent === "0"){
+    header.textContent = e.target.textContent;
+  }
+  else{
+    header.textContent = `${header.textContent}${e.target.textContent}`;
+  }
+}
+
+function displayOperator(e){
+  if (!operation.operator){
+    operation.first = header.textContent;
+    operation.operator = e.target.textContent;
+    header.textContent = `${header.textContent}${e.target.textContent}`;
+  }
+  else {
+    if ([...header.textContent].includes("*")){
+      const [one, two] = header.textContent.split("*");
+      operation.second = two;
+      header.textContent = operate(operation.first, operation.second, operation.operator);
     }
+  }
+}
+
+function clearOperation(){
+  operation.first = 0;
+  operation.second = 0;
+  operation.operator = "";
 }
 
 function clear(e) {
   header.textContent = "0";
+  clearOperation();
 }
 
 numbers.forEach((number) => {
-  number.addEventListener("click", display);
+  number.addEventListener("click", displayNumber);
 });
+
+operators.forEach((operator) => {
+  operator.addEventListener("click", displayOperator)
+})
 
 clearButton.addEventListener("click", clear);
